18b9fbfb0c9371daff5f865ab9ff86e727aa3c6e
JasmeetChatrath <Jasmeet.Chatrath@gmail.com>
2016-06-20 15:19:54 -0700

    Challenge 2

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,24 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    @sodas.each do |soda|
+      if soda.brand == soda_brand
+        return soda
+      else
+        return nil
+      end
+    end
   end
 
   def sell(soda_brand)
+    if @sodas.include?(soda_brand)
+      @cash = @cash + @price
+      @sodas.delete(soda_brand)
+    end
   end
 
 end
