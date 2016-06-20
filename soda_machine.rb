6c9187afa2a505f3f5ebd9d6fb25de71e4fdb9cd
Hayden Langelier <haydenlangelier@utexas.edu>
2016-06-20 15:19:13 -0700

    done

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,15 +1,24 @@
 class SodaMachine
   attr_reader :sodas, :cash
-
+  @@num_sodas=[]
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
+    @@num_sodas<<self
   end
 
   def current_inventory_count
+   @sodas.count
   end
 
   def find_soda(soda_brand)
+    
+    if @@num_sodas['brand'].include?(soda_brand)
+      return soda_brand
+    else
+      return 
+      nil
+    end
   end
 
   def sell(soda_brand)
