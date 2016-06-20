7081e568a864105d435c0b44b6c83536c13c8ac8
Aaron Kim <aaronkim1205@gmail.com>
2016-06-20 15:19:17 -0700

    Finish challenge

--- soda_machine.rb
+++ soda_machine.rb
@@ -12,17 +12,27 @@ class SodaMachine
 
   def find_soda(soda_brand)
     @sodas.find { |soda| soda.brand == soda_brand}
+
   end
 
   def sell(soda_brand)
     #when the soda is not available to be sold, return nil
     #when the soda is available to be sold, add the price of the soda sold to the cash
     #and remove the sold soda from the machine
-    @soda
-    if find_soda?
-      return nil
+    # if @sodas.empty?
+    #   return nil
+    # end
+    # if !@sodas.empty?
+    #   return nil
+    # end
+    soda = find_soda(soda_brand)
+    @cash += soda.price
+    if soda
+      @sodas.delete(soda)
+    else
+      nil
     end
 
   end
-
 end
+
