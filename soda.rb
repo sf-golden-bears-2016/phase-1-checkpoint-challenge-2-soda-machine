9dd81963890d14eaee6a30af7eb96e205e3b4a55
Hayden Langelier <haydenlangelier@utexas.edu>
2016-06-20 16:44:19 -0700

    updating find function

--- soda.rb
+++ soda.rb
@@ -7,3 +7,4 @@ class Soda
   end
 end
 
+
--- soda_machine.rb
+++ soda_machine.rb
@@ -1,27 +1,29 @@
 class SodaMachine
   attr_reader :sodas, :cash
-  @@num_sodas=[]
+
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
-    @@num_sodas<<self
+
   end
 
   def current_inventory_count
-   @sodas.count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
-    
-    if @@num_sodas['brand'].include?(soda_brand)
-      return soda_brand
-    else
-      return 
-      nil
+    @sodas.each do |soda|
+      if soda.brand == soda_brand
+       return soda
+      end
     end
+    return nil
   end
 
   def sell(soda_brand)
   end
 
 end
+
+
+
