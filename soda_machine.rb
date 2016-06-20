321f5c820ac940b436c0427c7a24330e8a34e033
Oshoke Maiki <oshokemaiki@gmail.com>
2016-06-20 15:19:08 -0700

    Add find_soda method, tests not working

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,19 +7,35 @@ class SodaMachine
   end
 
   def current_inventory_count
-    count_array = []
-    count_array << @sodas
-    count_array.length
+     @sodas.length
   end
+
+#A soda machine will be able to find a single soda with a given brand. This method will be called SodaMachine#find_soda.
+
 
   def find_soda(soda_brand)
-  end
-
-  def sell(soda_brand)
+    @sodas.each do |soda|
+      if soda.brand == soda_brand
+        return soda
+      end
+   end
+   return nil
   end
 
 end
 
+#A soda machine will be able to sell a soda with a given brand. When a soda is sold, it should be removed from the soda machine and the price of the soda should be added to the soda machine's cash This method will be called SodaMachine#sell.
+  def sell(soda_brand)
+    @sodas.each do |soda|
+    @sodas.delete
+  end
 
-machine = SodaMachine.new
-p machine.current_inventory_count
+  end
+
+# end
+
+
+# machine = SodaMachine.new
+# p machine.current_inventory_count
+# # p machine.find_soda("Pepsi")
+# # p machine.sell
