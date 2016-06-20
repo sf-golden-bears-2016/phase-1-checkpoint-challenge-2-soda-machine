434697639dc261f6934fb67bd507694ea1811ad9
Kenneth Olson <email": null>
2016-06-20 15:18:51 -0700

    complete 1 method

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,23 @@ class SodaMachine
   end
 
   def current_inventory_count
+    sodas.count
   end
 
   def find_soda(soda_brand)
+     @sodas.each do |soda|
+      if soda == soda.brand
+        return soda_brand
+      else
+        nil
+      end
+    end
   end
 
   def sell(soda_brand)
   end
 
 end
+
+#soda = SodaMachine.new(brand: 'Mountain Dew', price: 0.75)
+#p soda.sodas
