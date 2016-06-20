4e7f75f38928bd93c504d6c52664bf983f04b917
Marty McLaughlin <martymclaugh@gmail.com>
2016-06-20 14:32:54 -0700

    add class variable to keep inventory

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,12 +1,14 @@
 class SodaMachine
   attr_reader :sodas, :cash
-
+  @@inventory_count
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
+    @@inventory_count += 1
   end
 
   def current_inventory_count
+    @@inventory_count
   end
 
   def find_soda(soda_brand)
