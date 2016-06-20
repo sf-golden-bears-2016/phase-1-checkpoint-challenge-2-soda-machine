6d9f8f5079a547a2c33dce0a59bbaec472d5356b
Max Bigras <mbigras22@gmail.com>
2016-06-20 14:51:38 -0700

    add SodaMachine functionality for checkpoint challenge

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,26 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.select { |soda| soda.brand == soda_brand }.first
   end
 
   def sell(soda_brand)
-  end
+    # puts "@cash.class:"
+    # p @cash.class
+    soda = find_soda(soda_brand)
+    if soda != nil
+      @cash += soda.price
+      @sodas.delete(soda)
+    else
+      return nil
+    end
 
+    # puts "object_id for soda:"
+    # p soda.object_id
+
+  end
 end
