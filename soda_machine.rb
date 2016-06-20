8ae98c230dd58516aa0999876d00b38600fb3d04
Alexander Pellas <email": null>
2016-06-20 15:19:30 -0700

    Checkpoint Challenge.

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,24 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    if @sodas.include? soda_brand
+      @sodas[soda_brand]
+    else
+      nil
+    end
   end
 
   def sell(soda_brand)
+    if find_soda(soda_brand)
+      @cash += price[soda_brand]
+      @sodas -= soda_brand
+    else
+      nil
+    end
   end
 
 end
