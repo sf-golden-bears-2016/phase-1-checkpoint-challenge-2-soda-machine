1c2c65a60564cf466ae200b3e7ae4b505df4323c
Melanie <email": null>
2016-06-20 15:18:45 -0700

    Mountain Lion

--- soda_machine.rb
+++ soda_machine.rb
@@ -11,10 +11,19 @@ class SodaMachine
   end
 
   def find_soda(soda_brand)
+    @sodas.index { |x| x == soda_brand}
 
+    # @sodas[:sodas].find_index(soda_brand)
+    # if @sodas.include?(soda_brand)
+    #   @sodas
+    # else
+    #   nil
+    # end
   end
 
   def sell(soda_brand)
   end
 
 end
+
+
