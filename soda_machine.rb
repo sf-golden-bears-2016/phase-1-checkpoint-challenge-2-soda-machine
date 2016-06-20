7a4e831236ea15159b755644c32101d551d7c8bf
Mac Roche <email": null>
2016-06-20 15:21:17 -0700

    finishes checkpoint challenge

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,7 @@
+# Update the SodaMachine class in soda_machine.rb to make the tests pass.
+
+# Do not modify the tests and do not modify the Soda class
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -7,12 +11,27 @@ class SodaMachine
   end
 
   def current_inventory_count
+      return sodas.count
   end
 
   def find_soda(soda_brand)
+
+    if self.sodas.include(soda_brand.to_sym)
+      return true
+    else
+      return nil
+    end
+
   end
 
   def sell(soda_brand)
+    if sodas.find_soda == nil
+      return nil
+    else
+
+    end
+
+
   end
 
 end
