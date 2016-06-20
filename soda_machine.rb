37e1ce8c0c1abb58e5bfe0e88f21dc400015df70
Taki Vlasakakis <taki.vlasakakis@gmail.com>
2016-06-20 15:14:14 -0700

    added two methods in quiz

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,18 @@ class SodaMachine
   end
 
   def current_inventory_count
+  	sodas.length
   end
 
   def find_soda(soda_brand)
+# When they input a value of a key/value pair as a string, it should return the hash name
+	sodas.index(soda_brand)
   end
 
   def sell(soda_brand)
+  	
   end
 
 end
+
+
