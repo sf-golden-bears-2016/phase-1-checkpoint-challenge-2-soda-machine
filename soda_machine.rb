577be5f9989e4088c95be5217ea62a9eb110112a
Marty McLaughlin <martymclaugh@gmail.com>
2016-06-20 15:19:08 -0700

    get code to pass 6 out of 8 tests

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,10 +1,11 @@
 class SodaMachine
   attr_reader :sodas, :cash
-  @@inventory_count
+
+  @@inventory_count = 0
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
-    @@inventory_count += 1
+    @@inventory_count += :sodas.length - 1
   end
 
   def current_inventory_count
@@ -12,9 +13,29 @@ class SodaMachine
   end
 
   def find_soda(soda_brand)
+    @sodas.each do |soda|
+      if soda.brand == soda_brand
+        return soda
+      else
+        return nil
+      end
+    end
   end
 
+
   def sell(soda_brand)
+    @sodas.keep_if { |soda| soda == soda_brand}
+    @sodas.each do |soda|
+      if soda.brand == soda_brand
+        SodaMachine.cash += soda.price
+      else
+        return nil
+      end
+    end
+    # end
+    # @cash += soda.cash
+      
   end
 
 end
+
