b02153c4b388d8610eea1d2579c50b458223c5e8
je-lopez <je.lopez@me.com>
2016-06-20 14:55:09 -0700

    completes first two methods

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,5 @@
+require_relative "soda"
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -7,12 +9,18 @@ class SodaMachine
   end
 
   def current_inventory_count
+    sodas.count
   end
 
   def find_soda(soda_brand)
+    sodas.each do |soda|
+      return soda if soda.brand == soda_brand
+    end
+    return nil
   end
 
   def sell(soda_brand)
+    
   end
 
 end
