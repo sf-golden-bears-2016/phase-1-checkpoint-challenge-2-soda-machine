71b245098dd39127ad3eb0abcde2270a8d488a6f
Aaron Kim <aaronkim1205@gmail.com>
2016-06-20 14:44:49 -0700

    When the soda is available to be sold

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,22 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.find { |soda| soda.brand == soda_brand}
   end
 
   def sell(soda_brand)
+    #when the soda is not available to be sold, return nil
+    #when the soda is available to be sold, add the price of the soda sold to the cash
+    #and remove the sold soda from the machine
+    @soda
+    if find_soda?
+      return nil
+    end
+
   end
 
 end
