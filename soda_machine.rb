f546460756b7bc61db33bba1a3b92bbea7ec1bde
Taki Vlasakakis <taki.vlasakakis@gmail.com>
2016-06-20 15:18:42 -0700

    fuck you

--- soda_machine.rb
+++ soda_machine.rb
@@ -11,12 +11,18 @@ class SodaMachine
   end
 
   def find_soda(soda_brand)
-# When they input a value of a key/value pair as a string, it should return the hash name
-	sodas.index(soda_brand)
+# When they input a value of a key/value pair as a string, it should return the hash name which is an instance of a class
+
+	until sodas == "soda_brand"
+		if 	sodas == "soda_brand"
+			sodas.index(soda_brand)
+		end
+	end
+
   end
 
   def sell(soda_brand)
-  	
+
   end
 
 end
