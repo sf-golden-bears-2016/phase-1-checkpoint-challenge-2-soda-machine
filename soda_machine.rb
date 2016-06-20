069f8e4be008a6ff06993df9db567a516b6579e6
Simon Nim <simonnim916@gmail.com>
2016-06-20 15:18:53 -0700

    soda branch inventory stuff

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,24 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas = @sodas.length
   end
 
   def find_soda(soda_brand)
+    if @sodas.values.include?(soda_brand) == true
+      return soda_brand
+    else
+      return nil
+    end
   end
 
   def sell(soda_brand)
+    if @sodas.include?(soda_brand) == true
+      @sodas.delete(soda_brand)
+      @cash + 1.00
+    else
+      return nil
+    end
   end
 
 end
