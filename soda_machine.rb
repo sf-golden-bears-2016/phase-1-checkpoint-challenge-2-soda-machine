93887363b80acacf33a9fb9885a6b659813dbb44
nick bushell <email": null>
2016-06-20 15:19:16 -0700

    monday checkpoint

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,5 @@
+require_relative 'soda'
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -7,12 +9,18 @@ class SodaMachine
   end
 
   def current_inventory_count
+    sodas.length
   end
 
   def find_soda(soda_brand)
+    if sodas.find #times up
+   sodas.find { |soda| soda.brand == soda.brand}
   end
 
   def sell(soda_brand)
+    if find_soda(soda_brand) != nil
+      sodas.find { |soda_brand| soda_brand.price + sodamachine.cash}
+      sodas.delete(soda_brand)
+    end
   end
-
 end
