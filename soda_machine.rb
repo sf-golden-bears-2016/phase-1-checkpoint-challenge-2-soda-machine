f36011de35e9a6ae4aec096d7f2c24cb884dc974
Hayden Langelier <haydenlangelier@utexas.edu>
2016-06-20 17:13:28 -0700

    alternate answers

--- soda_machine.rb
+++ soda_machine.rb
@@ -9,15 +9,18 @@ class SodaMachine
 
   def current_inventory_count
     @sodas.count
+    #sodas.length
   end
 
   def find_soda(soda_brand)
-    @sodas.each do |soda|
-      if soda.brand == soda_brand
-       return soda
-      end
-    end
-    return nil
+   @sodas.detect { |soda| soda.brand.include?(soda_brand)}
+
+    # @sodas.each do |soda|
+    #   if soda.brand == soda_brand
+    #    return soda
+    #   end
+    # end
+    # return nil
   end
 
   def sell(soda_brand)
