7be844baf9a3ad463029f76921159a9665a244dd
David <email": null>
2016-06-25 17:25:53 -0700

    adds all methods

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,5 +1,8 @@
+require_relative 'soda'
+
 class SodaMachine
-  attr_reader :sodas, :cash
+  attr_reader :sodas
+  attr_accessor :cash
 
   def initialize(args = {})
     @sodas = args[:sodas]
@@ -7,12 +10,36 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+        soda = @sodas.find { |s| s.brand == soda_brand }
   end
 
   def sell(soda_brand)
+    soda = @sodas.find { |s| s.brand == soda_brand }
+    if soda
+      @cash += soda.price
+      return @cash
+    end
   end
 
 end
+
+# driver
+# pepsi = Soda.new(brand: "pepsi", price: 1.00)
+# mountain_dew = Soda.new(brand: "mountain dew", price: 1.00)
+# sodamachine = SodaMachine.new(sodas: [pepsi, mountain_dew] )
+
+#  sodamachine.find_soda('coke')
+
+# coke_zero = Soda.new(brand: 'Coke Zero', price: 1.00)
+# soda_machine = SodaMachine.new(sodas: [coke_zero], cash: 1.00)
+
+# p soda_machine.sell('Coke Zero')
+
+
+
+
+
